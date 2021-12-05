4d9ce66f0f60a9263a839fe0013fab8a80c25941
Edhar Prokudin <prokudin.ed@gmail.com>
2021-12-06 00:13:44 +0200

    qwerty

--- script.js
+++ script.js
@@ -1,4 +1,50 @@
-const tasks = [ 
-    { name: 'Learn HTML', checked: true }, 
-    { name: 'Learn JS', checked: false },
+
+function forEach(array, callback) {
+    for(let i = 0; i < array.length; i++) {
+        callback(array[i], i)
+    };
+};
+
+
+function map(array, callback) {
+    const result = [];
+
+    forEach(array, (arr, index) => {
+        result.push(callback(arr, index))
+    })
+
+    return result;
+};
+
+const products = [
+    { id: 1, name: 'Pencil', price: 12 }, 
+    { id: 2, name: 'Pen', price: 12 }
 ];
+
+
+
+
+const price = 0;
+const productsList = document.getElementById('products');
+
+
+// productsList.innerHTML = price;
+
+
+const removeTask = (prod) => {
+    document.querySelector(`[data-task="${prod.id}"]`).remove();
+};
+
+const productsHtml = map(products, (prod) => {
+    return `
+        <li data-prod="${prod.id}">
+            <button>+</button>
+            <p>${prod.name}</p>
+            <p>${prod.price}</p>
+            <button >-</button>
+            <button onclick='removeTask(${JSON.stringify(prod)}'>X</button>
+        </li>
+    `
+});
+
+productsList.innerHTML = productsHtml;
