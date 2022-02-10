94c45030b5f770ed26e81108a63c4ca33e9baf52
Edhar Prokudin <prokudin.ed@gmail.com>
2022-02-10 22:25:09 +0200

    up

--- main.js
+++ main.js
@@ -14,15 +14,71 @@ button.addEventListener('click', () => {
     const deleteBtn = document.createElement('button');
     deleteBtn.classList.add('margin-left', 'red-text');
     deleteBtn.innerText = 'Удалить';
-    newLi.append(deleteBtn);
+    todoList.append(deleteBtn);
 
+    todoList.addEventListener('click', (event) => {
+        let target = event.target;
 
-    deleteBtn.addEventListener('click', () => {
-        newLi.remove();
-        deleteBtn.remove();
-    });
-
+        if (target == deleteBtn) {
+            newLi.remove();
+            deleteBtn.remove();           
+        }
+    })
+    
 });
 
+todoList.addEventListener('click', (event) => {
+    let deleteBtn = document.querySelector('button')
+    if (event.target == deleteBtn) return
+    alert(event.target.innerHTML)
+});
+
+// Методы JS
+const numbersArr = [1, 2, 3, 4, 5];
+const chaosArr = ['a', true, 3, 5, 5, 6, 7, '>'];
 
 
+// forEach
+function forEach(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        callback(arr[i], i)
+    }
+}
+
+
+// findIndex
+function findIndex (arr, elem) {
+    let result = []
+
+    forEach(arr, (item, index) => {
+        if (item == elem) {
+            result += index
+        }
+    })
+
+    return result
+}
+
+console.log(findIndex(numbersArr, 2)) // 1 (index)
+
+
+
+// FIND
+function find (arr, num) {
+    num
+    let result = []
+
+    forEach(arr, (item) => {
+        if (item == num) {
+            result += item
+        }
+    })
+
+    if (result === []) {
+        result = 'hello'
+    }
+
+    return result
+}
+
+console.log(find(numbersArr, 2)) //
